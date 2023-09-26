6c004c2489128b7e8536c2accc73ce8bc988e144
nevadenstia <neva.denstia@ui.ac.id>
2023-09-26 16:09:53 +0700

    Tugas 4

--- main/models.py
+++ main/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Main(models.Model):
-    name = models.CharField(max_length=255)
     npm = models.IntegerField(default=0)
     kelas = models.TextField()
 
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     item_name = models.CharField(max_length=255)
     image = models.URLField(max_length=1000, default="insert image url here")
     amount = models.IntegerField(default=0)
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from main.forms import ProductForm
 from django.urls import reverse
@@ -6,20 +6,27 @@ from main.models import Product
 from django.http import HttpResponse
 from django.core import serializers
 from django.db.models import Sum
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
 
 # Create your views here.
 
+@login_required(login_url='/login')
+
 def show_main(request):
-    products = Product.objects.all()
-    total_stock = products.aggregate(total_stock=Sum('amount'))['total_stock'] or 0
+    total_stock = Product.objects.all().aggregate(total_stock=Sum('amount'))['total_stock'] or 0
+    products = Product.objects.filter(user=request.user)
 
     context = {
-        'name' : 'Neva Denstia Shabira',
+        'name' : request.user.username,
         'npm' : '2206083073',
         'kelas' : 'PBP B',
         'total_stock' : total_stock,
         'products' : products,
+        'last_login' : request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
@@ -28,11 +35,46 @@ def create_product(request):
     form = ProductForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        product = form.save(commit=False)
+        product.user = request.user
+        product.save()
         return HttpResponseRedirect(reverse('main:show_main'))
 
     context = {'form': form}
     return render(request, "create_product.html", context)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
 
 def show_xml(request):
     data = Product.objects.all()
